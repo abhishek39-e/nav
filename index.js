3c523a58f053e292d1a83a3475edf1adf3ab3625
Abhishek kaushal <abhishekkaush39@gmail.com>
2025-07-23 10:52:46 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+let num = document.querySelectorAll('li')
+num.forEach((e) => {
+  e.addEventListener('click', () => {
+    num.forEach((li) => li.classList.remove('active'))
+    console.log(e.innerText)
+    e.classList.add('active')
+    let getClass = e.innerText.toLowerCase()
+    console.log(getClass)
+    document
+      .querySelector(`.${getClass}`)
+      .scrollIntoView({ behavior: 'smooth' })
+  })
+})
